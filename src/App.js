eb5c87cd8c82e436f46405ee4778473a74bc02ac
Ilya Misyura <ilya.mi@digitrade.co.il>
2022-06-02 16:45:04 +0300

    add routes and dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import Home from './pages/Home';
+import Manufacturers from './pages/Manufacturers';
+import Categories from './pages/Categories';
+
 
 function App() {
+
   return (
     <div className="App">
-      hello
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/manufacturers" element={<Manufacturers />}/>
+          <Route path="/categories" element={<Categories />}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
